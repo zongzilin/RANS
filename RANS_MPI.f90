536201f95c2ac72bd32889d1a494c5d7e5bdb8ac
zongzilin <77555077+zongzilin@users.noreply.github.com>
2021-11-03 13:15:57 +1100

    Works for different nhx nhy

--- RANS_MPI.f90
+++ RANS_MPI.f90
@@ -1,6 +1,8 @@
 PROGRAM RANS_MPI
     USE MPI 
     USE RANS_lib 
+    USE readin 
+    USE omp_lib
 
     IMPLICIT NONE 
 
@@ -9,24 +11,25 @@ PROGRAM RANS_MPI
     ! Domain
     INTEGER :: n, nhx, nhy 
     REAL(KIND = 8) :: x, y, hx, hy
+    REAL(KIND = 8), allocatable :: x_tec(:), y_tec(:)
     REAL(KIND = 8), ALLOCATABLE :: meshY(:)
     REAL(KIND = 8) :: dt 
 
     ! Velocity field 
-    REAL(KIND = 8), ALLOCATABLE :: U(:,:),  V(:,:), Unew(:,:), Vnew(:,:)
+    REAL(KIND = 8), ALLOCATABLE :: U(:,:),  V(:,:), Unew(:,:), Vnew(:,:), Uout(:,:), Vout(:,:), Pout(:,:)
 
     ! Pressure field
     REAL(KIND = 8), ALLOCATABLE :: P(:,:), Pc(:,:)
 
     ! Residual/Divergence field 
     REAL(KIND = 8) :: div_sum
-    REAL(KIND = 8), ALLOCATABLE :: residual(:,:), div(:,:)
+    REAL(KIND = 8), ALLOCATABLE :: residual(:,:), div(:,:), div_gl(:,:)
 
     ! Turbulence model (eddy viscosity)
     REAL(KIND = 8), ALLOCATABLE :: delY_u(:,:), delYY_u(:,:), delYX_u(:,:),ml(:,:), tmp(:,:)
 
     ! Iteration/relaxation factor
-    INTEGER :: n_count, p_count, i, j, k, ii, jj 
+    INTEGER :: n_count, p_count, i, j, k, ii, jj, kk
     REAL(KIND = 8) :: w, relx_pc
 
     ! Error/ Steady state criteria
@@ -34,8 +37,12 @@ PROGRAM RANS_MPI
 
     ! Domain Decomposition
     INTEGER :: no_node 
-    INTEGER :: ll, lh ! Local Low, Local High 
+    INTEGER :: ll, lh, lh_prs, ll_prs ! Local Low, Local High, local pressure low/high  
     INTEGER, ALLOCATABLE :: lglel(:) ! Local to GLoblal ELement
+
+    ! Solver timer
+    REAL :: t_prsslv1, t_prsslv2
+    DOUBLE precision :: t_solve1, t_solve2, t2solve
 
     ! Data IO
     INTEGER :: UNIT 
@@ -55,6 +62,10 @@ PROGRAM RANS_MPI
     INTEGER :: ierr, pid, np, tag 
     INTEGER(KIND = 4) :: stat(MPI_STATUS_SIZE, 8), istat(MPI_STATUS_SIZE)
     INTEGER :: left, right, req(8)
+    INTEGER, ALLOCATABLE :: displacement(:)
+
+    ! CG initialise
+    REAL(kind = 8), ALLOCATABLE :: aw(:,:), ae(:,:), an(:,:), as(:,:), ap(:,:), b(:,:)
 
     tag = 1
     CALL MPI_Init(ierr)
@@ -68,14 +79,16 @@ PROGRAM RANS_MPI
     nu = 0.001
     U_in = 60.0
 
-    n = 4 
-    x = 8 ! Domain length
-    y = 0.1
-    hx = x/2**n 
-    hy = y/2**n 
-    nhx = NINT(x/hx) + 2
-    nhy = NINT(y/hy) + 2
+    ! n = 4 
+    ! x = 8 ! Domain length
+    ! y = 0.1
+    ! hx = x/2**n 
+    ! hy = y/2**n 
+    ! nhx = NINT(x/hx) + 2
+    ! nhy = NINT(y/hy) + 2
     dt = 0.00001 ! may work for bigger dt 
+
+    call userinp(x,y,hx,hy,nhx,nhy,nu,rho,U_change_max,resid_pc_max)
 
     call Decompose(nhx,PID,np,no_node,lglel)
 
@@ -90,18 +103,29 @@ PROGRAM RANS_MPI
 
     ALLOCATE(U(nhy,nhx))
     ALLOCATE(Unew(nhy,nhx))
+    ALLOCATE(Uout(nhy,nhx))
     ALLOCATE(V(nhy,nhx))
     ALLOCATE(Vnew(nhy,nhx))
+    ALLOCATE(Vout(nhy,nhx))
     ALLOCATE(P(nhy,nhx))
     ALLOCATE(Pc(nhy,nhx))
+    ALLOCATE(Pout(nhy,nhx))
     ALLOCATE(residual(nhy,nhx))
     ALLOCATE(div(nhy,nhx))
+    ALLOCATE(div_gl(nhy,nhx))
     ALLOCATE(meshY(nhy))
     ALLOCATE(delY_u(nhy,nhx))
     ALLOCATE(delYY_u(nhy,nhx))
     ALLOCATE(delYX_u(nhy,nhx))
     ALLOCATE(ml(nhy,nhx))
     ALLOCATE(tmp(nhy,nhx))
+
+    ALLOCATE(ae(nhx*nhy,nhx*nhy))
+    ALLOCATE(aw(nhx*nhy,nhx*nhy))
+    ALLOCATE(ap(nhx*nhy,nhx*nhy))
+    ALLOCATE(an(nhx*nhy,nhx*nhy))
+    ALLOCATE(as(nhx*nhy,nhx*nhy))
+    ALLOCATE(b(nhx,nhy))
 
     CALL linspace(-hy/2,0.1+hy/2,hy,meshY)
 
@@ -127,26 +151,25 @@ PROGRAM RANS_MPI
     w = 1.9
     relx_pc = 1.5
 
-    !################### TURN THIS INTO SUBROUNTINE call it mpicart #################################!
-    dims = (/0,0/)
-    CALL MPI_Dims_create(np, 1, dims, ierr)
+    call init_mpi(np,neighbours_ranks,cart_comm)
 
-    ! Creates communicator on cartesian topology
-    reorder = .TRUE.
-    periods = (/.FALSE., .FALSE./)
-    CALL MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, reorder, cart_comm, ierr)
+    IF ((np .GT. nhx) .OR. (np .GT. nhy)) then 
+        IF(PID == 0) write(*,*) 'Too little cells for too many PIDs'
+        call MPI_FINALIZE(ierr)
+        return 
+    end IF 
 
-    CALL MPI_Cart_shift(cart_comm, 1, 1, neighbours_ranks(LEFTc), neighbours_ranks(RIGHTc), ierr)
-
-    !write(*,*) 'this is PID', PID, 'with right neighour', neighbours_ranks(3), ' and left neighour ',neighbours_ranks(2)
-
-    !##############################################################################################!
+    left = neighbours_ranks(2)
+    right = neighbours_ranks(3)
 
     CALL mxlen(nhx,nhy,y,meshY,ml) ! Mixing length uniform for all PID 
 
-    !DO WHILE (U_change .GE. U_change_max)
-    DO ii = 1,1
-         n_count = n_count + 1
+    t_solve1 = MPI_Wtime()
+
+
+    DO WHILE (U_change .GE. U_change_max)
+    !DO jj = 1,1
+        n_count = n_count + 1
 
     !     ! ############## MADE THIS SECTION A SUBROUNTINE (CALL IT eddy_visc) ############################!
          DO i = 2,nhy - 1
@@ -158,20 +181,21 @@ PROGRAM RANS_MPI
          delY_u(1,2:nhx-1) = 0
          delY_u(2:nhy,1) = -delY_u(2:nhy,2)
          delY_u(2:nhy,nhx) = -delY_u(2:nhy,nhx-1)
-
-        call sendTo(PID,np,left,right) ! SHOULD CHANGE TO MPI CART SHIFT
-
-        left = neighbours_ranks(2)
-        right = neighbours_ranks(3)
-
-        CALL MPI_iRecv(delY_u(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(delY_u(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(delY_u(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(delY_u(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
         CALL MPI_waitall(4,req,stat,ierr)
 
-        tmp = ml**2*ABS(delY_u)*delY_u
+        tmp = ml**2*ABS(delY_u(:,ll:lh))*delY_u(:,ll:lh)
+        DO i = 2,nhy - 1
+            DO j = ll,lh 
+                tmp(i,j) = ml(i,j)**2*ABS(delY_u(i,j))*delY_u(i,j)
+            end DO
+        end DO
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(tmp,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+
+        CALL MPI_waitall(4,req,stat,ierr)
 
         DO i = 2,nhy - 1
             DO j = ll,lh
@@ -179,14 +203,10 @@ PROGRAM RANS_MPI
             end DO
         end DO
 
-        CALL MPI_iRecv(delYY_u(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(delYY_u(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(delYY_u(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(delYY_u(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delYY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
         CALL MPI_waitall(4,req,stat,ierr)
-
-  
 
         DO i = 2,nhy - 1
             DO j = ll,lh
@@ -194,12 +214,11 @@ PROGRAM RANS_MPI
             end DO
         end DO
 
-        CALL MPI_iRecv(delYX_u(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(delYX_u(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(delYX_u(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(delYX_u(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delYX_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
         CALL MPI_waitall(4,req,stat,ierr)
+
 
     !     !############################################################################################!
 
@@ -219,12 +238,14 @@ PROGRAM RANS_MPI
         Unew(1,2:nhx-1)=-Unew(2,2:nhx-1)+2*U_in;
         Unew(2:nhy,1)=-Unew(2:nhy,2);
         Unew(2:nhy,nhx)=-Unew(2:nhy,nhx-1);
-        U_change= MAXVAL(ABS((Unew-U)/dt)) ! MPI_Allreduce?
 
-        CALL MPI_iRecv(Unew(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(Unew(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(Unew(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(Unew(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+        
+        U_change= MAXVAL(ABS((Unew(:,ll:lh)-U(:,ll:lh))/dt)) ! MPI_Allreduce?
+
+        CALL MPI_Allreduce(U_change, U_change, 1, MPI_DOUBLE_PRECISION, MPI_SUM, cart_comm, ierr)
+
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(Unew,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
         CALL MPI_waitall(4,req,stat,ierr)
 
@@ -238,17 +259,16 @@ PROGRAM RANS_MPI
                 -dt*V(i,j)/(2*hy)*(V(i,j+1)-V(i,j-1))+dt*delYX_u(i,j))
             end DO
         end DO
-
+        
         ! Update Boundary Condition 
         Vnew(nhy,:)=Vnew(nhy-1,:);
         Vnew(1,:)=-Vnew(2,:);
         Vnew(:,1)=-Vnew(:,2);
         Vnew(:,nhx)=-Vnew(:,nhx-1);
 
-        CALL MPI_iRecv(Vnew(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(Vnew(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(Vnew(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(Vnew(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+        
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(Vnew,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
         CALL MPI_waitall(4,req,stat,ierr)
 
@@ -260,61 +280,68 @@ PROGRAM RANS_MPI
             end DO 
         end DO
 
-        CALL MPI_iRecv(div(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-        CALL MPI_iSend(div(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-        CALL MPI_iRecv(div(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-        CALL MPI_iSend(div(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
+        
+
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(div,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)     
 
         CALL MPI_waitall(4,req,stat,ierr)
 
-        if(PID == 0) write(*,*) div(:,2)
-
         div_sum = sum(abs(div))/((nhx-2)*(nhy-2))
-
 
         resid_pc = 1
         p_count = 0 
         Pc = 0.0
 
-        !DO WHILE ((resid_pc .GT. resid_pc_max) .AND. (p_count .LT. 100)) 
-        DO jj = 1,1
+
+        IF (PID == 0) then 
+
+            div_gl(:,ll:lh) = div(:,ll:lh)
+            DO kk = 1, np - 1
+                call Decompose(nhx,kk,np,no_node,lglel)
+                ll_prs = lglel(1) 
+                lh_prs = lglel(size(lglel))
+                if (kk == np - 1) lh_prs = lh_prs - 1
+                CALL MPI_Recv(div_gl(:,ll_prs:lh_prs),(lh_prs-ll_prs+1)*nhy, MPI_DOUBLE_PRECISION,kk,tag,cart_comm,istat,ierr)
+            end DO
+        ELSE 
+            CALL MPI_Send(div(:,ll:lh),(lh-ll+1)*nhy,MPI_DOUBLE_PRECISION,0,tag,cart_comm,ierr)
+        end IF
+
+        call MPI_Bcast(div_gl,nhx*nhy,MPI_DOUBLE_PRECISION,0,cart_comm,ierr)
+
+
+
+        DO WHILE ((resid_pc .GT. resid_pc_max) .AND. (p_count .LT. 100)) 
             p_count = p_count + 1
             DO i = 2,nhy - 1
-                DO j = ll,lh
-                    residual(i,j) = -1/(hx*hx)*(Pc(i+1,j)-2.*Pc(i,j)+Pc(i-1,j)) &
-                                    -1/(hy*hy)*(Pc(i,j+1)-2.*Pc(i,j)+Pc(i,j-1)) &
-                                    +(1./dt)*div(i,j)
+                DO j = 2, nhx - 1 
+                        residual(i,j) = -1/(hx*hx)*(Pc(i+1,j)-2.*Pc(i,j)+Pc(i-1,j)) &
+                                        -1/(hy*hy)*(Pc(i,j+1)-2.*Pc(i,j)+Pc(i,j-1)) &
+                                        +(1./dt)*div_gl(i,j)
+                        Pc(i,j)= (1/(-2/(hx*hx)-2/(hy*hy))*residual(i,j))*relx_pc+Pc(i,j);
                 end DO
             end DO
-
-            CALL MPI_iRecv(div(1:nhy,ll-1),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(2),ierr)
-            CALL MPI_iSend(div(1:nhy,ll),nhy,MPI_DOUBLE_PRECISION,left,tag,MPI_COMM_WORLD,req(1),ierr)
-            CALL MPI_iRecv(div(1:nhy,lh+1),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(4),ierr)
-            CALL MPI_iSend(div(1:nhy,lh),nhy,MPI_DOUBLE_PRECISION,right,tag,MPI_COMM_WORLD,req(3),ierr)
-    
-            CALL MPI_waitall(4,req,stat,ierr)
-
-            
-        
-            Pc(1,:)=Pc(2,:);
-            Pc(:,1)=Pc(:,2);
-            Pc(:,nhx)=Pc(:,nhx-1);
-            Pc(nhy,:)=0;
-            
-            resid_pc = sum(abs(residual))/((nhx-2)*(nhy-2))
-
-            !if (PID == 1) write(*,*) residual(:,ll)
+                Pc(1,:)=Pc(2,:)
+                Pc(:,1)=Pc(:,2)
+                Pc(:,nhx)=Pc(:,nhx-1)
+                Pc(nhy,:)=0
 
         end DO
+ 
 
+        CALL MPI_Bcast(Pc,nhx*nhy,MPI_DOUBLE_PRECISION,0,cart_comm,ierr)
+
+        !call MPI_SEND_RECV(Pc,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+        CALL MPI_waitall(4,req,stat,ierr)
 
         ! Update Pressure
         P = P + Pc 
 
         DO i = 2,nhy - 1
             DO j = ll,lh
-                Unew(i,j)= Unew(i,j)-dt*(Pc(i+1,j)-Pc(i-1,j))/(2*hx)
-                Vnew(i,j)= Vnew(i,j)-dt*(Pc(i,j+1)-Pc(i,j-1))/(2*hy)
+                Unew(i,j)= Unew(i,j)-(dt/rho)*(Pc(i+1,j)-Pc(i-1,j))/(2*hx)
+                Vnew(i,j)= Vnew(i,j)-(dt/rho)*(Pc(i,j+1)-Pc(i,j-1))/(2*hy)
             end DO
         end DO
 
@@ -332,38 +359,68 @@ PROGRAM RANS_MPI
         U=Unew;
         V=Vnew;
 
-        ! if (PID == 0) then 
-        !         WRITE(*,'(a40)') '-----------------------------------------'
-        !         WRITE(*,'(a20,i20.1)') '# Current Iteration = ', n_count 
-        !         WRITE(*,'(a20,E20.6)') '# Current Delta U = ', U_change
-        ! end if 
-        
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(Unew,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+    
+        CALL MPI_waitall(4,req,stat,ierr)
+    
+	    ! MPI send and recv
+		CALL MPI_SEND_RECV(Vnew,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+    
+        CALL MPI_waitall(4,req,stat,ierr)  
+
+        if (PID == 0) then 
+                WRITE(*,'(a40)') '-----------------------------------------'
+                !WRITE(*,'(a20,i20.1)') '# Current Iteration = ', n_count
+                WRITE(*,'(a20,E20.6)') '# Current Delta U = ', U_change
+        end if 
 
     end DO
 
+    t_solve2 = MPI_Wtime()
+    t2solve = t_solve2 - t_solve1
+
+    ! Collect data to write to disc
+    IF (PID == 0) then 
+        Uout(:,ll:lh) = U(:,ll:lh)
+        DO kk = 1, np - 1
+            call Decompose(nhx,kk,np,no_node,lglel)
+            ll_prs = lglel(1) 
+            lh_prs = lglel(size(lglel))
+            if (kk == np - 1) lh_prs = lh_prs - 1
+            CALL MPI_Recv(Uout(:,ll_prs:lh_prs),(lh_prs-ll_prs+1)*nhy, MPI_DOUBLE_PRECISION,kk,tag,cart_comm,istat,ierr)
+        end DO
+    ELSE 
+        CALL MPI_Send(U(:,ll:lh),(lh-ll+1)*nhy,MPI_DOUBLE_PRECISION,0,tag,cart_comm,ierr)
+    end IF
+
+    call MPI_Bcast(Uout,nhx*nhy,MPI_DOUBLE_PRECISION,0,cart_comm,ierr)
+
     CALL MPI_FINALIZE(ierr)
 
-    ! ! Data IO
-    ! filename = 'Uout.dat'
-    ! UNIT = 1001 
+    !CALL write_to_screen(PID,nhx,nhy,n_count,t2solve)
 
-    ! OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
-    !     WRITE(UNIT,*) U 
-    ! CLOSE(UNIT)
+    ! Data IO
+    filename = 'Uout.dat'
+    UNIT = 1001 
 
-    ! filename = 'Vout.dat'
-    ! UNIT = 1001 
+    OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
+        WRITE(UNIT,*) Uout 
+    CLOSE(UNIT)
 
-    ! OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
-    !     WRITE(UNIT,*) V 
-    ! CLOSE(UNIT)
+    filename = 'Vout.dat'
+    UNIT = 1001 
 
-    ! filename = 'Pout.dat'
-    ! UNIT = 1001 
+    OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
+        WRITE(UNIT,*) V 
+    CLOSE(UNIT)
 
-    ! OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
-    !     WRITE(UNIT,*) P 
-    ! CLOSE(UNIT)
+    filename = 'Pout.dat'
+    UNIT = 1001 
+
+    OPEN(UNIT,FILE = filename, FORM = 'FORMATTED', STATUS = 'REPLACE')
+        WRITE(UNIT,*) P 
+    CLOSE(UNIT)
 
 
 
