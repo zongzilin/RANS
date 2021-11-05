07e83da3bf210123aee5053c2b815d4c2ca03b34
zongzilin <77555077+zongzilin@users.noreply.github.com>
2021-11-06 00:26:55 +1100

    Pressure jacobi

--- RANS_MPI.f90
+++ RANS_MPI.f90
@@ -11,7 +11,7 @@ PROGRAM RANS_MPI
     ! Domain
     INTEGER :: n, nhx, nhy 
     REAL(KIND = 8) :: x, y, hx, hy
-    REAL(KIND = 8), allocatable :: x_tec(:), y_tec(:)
+    REAL(KIND = 8), ALLOCATABLE :: x_tec(:), y_tec(:)
     REAL(KIND = 8), ALLOCATABLE :: meshY(:)
     REAL(KIND = 8) :: dt 
 
@@ -19,7 +19,8 @@ PROGRAM RANS_MPI
     REAL(KIND = 8), ALLOCATABLE :: U(:,:),  V(:,:), Unew(:,:), Vnew(:,:), Uout(:,:), Vout(:,:), Pout(:,:)
 
     ! Pressure field
-    REAL(KIND = 8), ALLOCATABLE :: P(:,:), Pc(:,:)
+    REAL(KIND = 8), ALLOCATABLE :: P(:,:), Pc(:,:), trial(:,:), trial_lag(:,:)
+    REAL(KIND = 8) :: prs_coeff
 
     ! Residual/Divergence field 
     REAL(KIND = 8) :: div_sum
@@ -74,82 +75,7 @@ PROGRAM RANS_MPI
 
 
     ! INITIALISE value 
-    ! ###########################  USE SUBROUTINE !!!!!! ###########################!
-    rho = 1
-    nu = 0.001
-    U_in = 60.0
-
-    ! n = 4 
-    ! x = 8 ! Domain length
-    ! y = 0.1
-    ! hx = x/2**n 
-    ! hy = y/2**n 
-    ! nhx = NINT(x/hx) + 2
-    ! nhy = NINT(y/hy) + 2
-    dt = 0.00001 ! may work for bigger dt 
-
-    call userinp(x,y,hx,hy,nhx,nhy,nu,rho,U_change_max,resid_pc_max)
-
-    call Decompose(nhx,PID,np,no_node,lglel)
-
-    ll = lglel(1) 
-    lh = lglel(size(lglel))
-
-    if (PID == 0) then 
-        ll = 2
-    end if
-
-    if (PID == np - 1) lh = lh - 1
-
-    ALLOCATE(U(nhy,nhx))
-    ALLOCATE(Unew(nhy,nhx))
-    ALLOCATE(Uout(nhy,nhx))
-    ALLOCATE(V(nhy,nhx))
-    ALLOCATE(Vnew(nhy,nhx))
-    ALLOCATE(Vout(nhy,nhx))
-    ALLOCATE(P(nhy,nhx))
-    ALLOCATE(Pc(nhy,nhx))
-    ALLOCATE(Pout(nhy,nhx))
-    ALLOCATE(residual(nhy,nhx))
-    ALLOCATE(div(nhy,nhx))
-    ALLOCATE(div_gl(nhy,nhx))
-    ALLOCATE(meshY(nhy))
-    ALLOCATE(delY_u(nhy,nhx))
-    ALLOCATE(delYY_u(nhy,nhx))
-    ALLOCATE(delYX_u(nhy,nhx))
-    ALLOCATE(ml(nhy,nhx))
-    ALLOCATE(tmp(nhy,nhx))
-
-    ALLOCATE(ae(nhx*nhy,nhx*nhy))
-    ALLOCATE(aw(nhx*nhy,nhx*nhy))
-    ALLOCATE(ap(nhx*nhy,nhx*nhy))
-    ALLOCATE(an(nhx*nhy,nhx*nhy))
-    ALLOCATE(as(nhx*nhy,nhx*nhy))
-    ALLOCATE(b(nhx,nhy))
-
-    CALL linspace(-hy/2,0.1+hy/2,hy,meshY)
-
-    U = 0.0
-    U(2:nhy - 1,1:2) = U_in 
-    Unew = U
-    V = 0.0
-    Vnew = V 
-    residual = 0.0 
-    div = 0.0
-    delY_u = 0.0
-    delYY_u = 0.0
-    delYX_u = 0.0
-    P = 0.0
-    P(:,1) = 1.0
-    Pc = P 
-
-    U_change = 1
-    U_change_max = 1e-4
-    resid_pc = 1
-    resid_pc_max = 1e-4
-
-    w = 1.9
-    relx_pc = 1.5
+    call valInit()
 
     call init_mpi(np,neighbours_ranks,cart_comm)
 
@@ -171,56 +97,7 @@ PROGRAM RANS_MPI
     !DO jj = 1,1
         n_count = n_count + 1
 
-    !     ! ############## MADE THIS SECTION A SUBROUNTINE (CALL IT eddy_visc) ############################!
-         DO i = 2,nhy - 1
-             DO j = ll,lh
-             delY_u(i,j) = (Unew(i+1,j)-Unew(i-1,j))/(2*hy)
-             end DO 
-         end DO
-         delY_u(nhy,:) = 0
-         delY_u(1,2:nhx-1) = 0
-         delY_u(2:nhy,1) = -delY_u(2:nhy,2)
-         delY_u(2:nhy,nhx) = -delY_u(2:nhy,nhx-1)
-		! MPI send and recv
-		CALL MPI_SEND_RECV(delY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
-
-        CALL MPI_waitall(4,req,stat,ierr)
-
-        tmp = ml**2*ABS(delY_u(:,ll:lh))*delY_u(:,ll:lh)
-        DO i = 2,nhy - 1
-            DO j = ll,lh 
-                tmp(i,j) = ml(i,j)**2*ABS(delY_u(i,j))*delY_u(i,j)
-            end DO
-        end DO
-        ! MPI send and recv
-		CALL MPI_SEND_RECV(tmp,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
-
-        CALL MPI_waitall(4,req,stat,ierr)
-
-        DO i = 2,nhy - 1
-            DO j = ll,lh
-                delYY_u(i,j) = (tmp(i+1,j)-tmp(i-1,j))/(2*hy);
-            end DO
-        end DO
-
-		! MPI send and recv
-		CALL MPI_SEND_RECV(delYY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
-
-        CALL MPI_waitall(4,req,stat,ierr)
-
-        DO i = 2,nhy - 1
-            DO j = ll,lh
-                delYX_u(i,j) = (tmp(i,j+1)-tmp(i,j-1))/(2*hx);
-            end DO
-        end DO
-
-		! MPI send and recv
-		CALL MPI_SEND_RECV(delYX_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
-
-        CALL MPI_waitall(4,req,stat,ierr)
-
-
-    !     !############################################################################################!
+    call eddy_visc(nhx,nhy,Unew,delY_u,ml,delYY_u,delYX_u,ll,lh,left,right,tag,cart_comm,req)
 
     !     ! U - Velocity solve
         DO i = 2,nhy - 1
@@ -238,9 +115,7 @@ PROGRAM RANS_MPI
         Unew(1,2:nhx-1)=-Unew(2,2:nhx-1)+2*U_in;
         Unew(2:nhy,1)=-Unew(2:nhy,2);
         Unew(2:nhy,nhx)=-Unew(2:nhy,nhx-1);
-
-        
-        U_change= MAXVAL(ABS((Unew(:,ll:lh)-U(:,ll:lh))/dt)) ! MPI_Allreduce?
+        U_change= MAXVAL(ABS((Unew(:,ll:lh)-U(:,ll:lh))/dt))
 
         CALL MPI_Allreduce(U_change, U_change, 1, MPI_DOUBLE_PRECISION, MPI_SUM, cart_comm, ierr)
 
@@ -259,14 +134,13 @@ PROGRAM RANS_MPI
                 -dt*V(i,j)/(2*hy)*(V(i,j+1)-V(i,j-1))+dt*delYX_u(i,j))
             end DO
         end DO
-        
+
         ! Update Boundary Condition 
         Vnew(nhy,:)=Vnew(nhy-1,:);
         Vnew(1,:)=-Vnew(2,:);
         Vnew(:,1)=-Vnew(:,2);
         Vnew(:,nhx)=-Vnew(:,nhx-1);
 
-        
         ! MPI send and recv
 		CALL MPI_SEND_RECV(Vnew,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
 
@@ -280,11 +154,10 @@ PROGRAM RANS_MPI
             end DO 
         end DO
 
-        
+        !if(PID == 0) write(*,*) div(:,2)
 
         ! MPI send and recv
 		CALL MPI_SEND_RECV(div,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)     
-
         CALL MPI_waitall(4,req,stat,ierr)
 
         div_sum = sum(abs(div))/((nhx-2)*(nhy-2))
@@ -292,10 +165,10 @@ PROGRAM RANS_MPI
         resid_pc = 1
         p_count = 0 
         Pc = 0.0
+        relx_pc = 1.6
 
 
         IF (PID == 0) then 
-
             div_gl(:,ll:lh) = div(:,ll:lh)
             DO kk = 1, np - 1
                 call Decompose(nhx,kk,np,no_node,lglel)
@@ -310,38 +183,41 @@ PROGRAM RANS_MPI
 
         call MPI_Bcast(div_gl,nhx*nhy,MPI_DOUBLE_PRECISION,0,cart_comm,ierr)
 
-
-
         DO WHILE ((resid_pc .GT. resid_pc_max) .AND. (p_count .LT. 100)) 
             p_count = p_count + 1
             DO i = 2,nhy - 1
-                DO j = 2, nhx - 1 
-                        residual(i,j) = -1/(hx*hx)*(Pc(i+1,j)-2.*Pc(i,j)+Pc(i-1,j)) &
-                                        -1/(hy*hy)*(Pc(i,j+1)-2.*Pc(i,j)+Pc(i,j-1)) &
-                                        +(1./dt)*div_gl(i,j)
-                        Pc(i,j)= (1/(-2/(hx*hx)-2/(hy*hy))*residual(i,j))*relx_pc+Pc(i,j);
+                DO j = ll,lh 
+                        trial(i,j) = trial_lag(i,j)*(1-relx_pc)+relx_pc*0.25*(div(i,j)/dt & 
+                                    - (trial_lag(i-1,j) + trial_lag(i+1,j))/hx**2 - &
+                                    (trial_lag(i,j-1)+trial_lag(i,j+1))/hy**2)*prs_coeff
                 end DO
             end DO
-                Pc(1,:)=Pc(2,:)
-                Pc(:,1)=Pc(:,2)
-                Pc(:,nhx)=Pc(:,nhx-1)
-                Pc(nhy,:)=0
+            
+            resid_pc = ABS(SUM(ABS(trial(:,ll:lh)))-SUM(ABS(trial_lag(:,ll:lh))))/((nhx-2)*(nhy-2))
+            CALL MPI_Allreduce(resid_pc, resid_pc, 1, MPI_DOUBLE_PRECISION, MPI_SUM, cart_comm, ierr)
+            CALL MPI_SEND_RECV(trial,nhy,nhx,ll,lh,left,right,tag,cart_comm,req) 
+            CALL MPI_SEND_RECV(trial_lag,nhy,nhx,ll,lh,left,right,tag,cart_comm,req) 
 
+            trial_lag = trial
         end DO
- 
 
-        CALL MPI_Bcast(Pc,nhx*nhy,MPI_DOUBLE_PRECISION,0,cart_comm,ierr)
-
-        !call MPI_SEND_RECV(Pc,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
-        CALL MPI_waitall(4,req,stat,ierr)
+        Pc = prs_coeff*trial+trial
+        
+        ! Pressure boundary conditions
+        Pc(1,:) = Pc(2,:)
+        Pc(:,1) = Pc(:,2)
+        Pc(:,nhx)=Pc(:,nhx-1)
+        Pc(nhy,:)=0
 
         ! Update Pressure
         P = P + Pc 
+        !if(PID == 0) write(*,*) trial(:,lh), ll, lh 
+        
 
         DO i = 2,nhy - 1
             DO j = ll,lh
-                Unew(i,j)= Unew(i,j)-(dt/rho)*(Pc(i+1,j)-Pc(i-1,j))/(2*hx)
-                Vnew(i,j)= Vnew(i,j)-(dt/rho)*(Pc(i,j+1)-Pc(i,j-1))/(2*hy)
+                Unew(i,j)= Unew(i,j)-dt*(Pc(i+1,j)-Pc(i-1,j))/(2*hx)
+                Vnew(i,j)= Vnew(i,j)-dt*(Pc(i,j+1)-Pc(i,j-1))/(2*hy)
             end DO
         end DO
 
@@ -398,7 +274,7 @@ PROGRAM RANS_MPI
 
     CALL MPI_FINALIZE(ierr)
 
-    !CALL write_to_screen(PID,nhx,nhy,n_count,t2solve)
+    CALL write_to_screen(PID,nhx,nhy,n_count,t2solve)
 
     ! Data IO
     filename = 'Uout.dat'
@@ -422,6 +298,154 @@ PROGRAM RANS_MPI
         WRITE(UNIT,*) P 
     CLOSE(UNIT)
 
+Contains
+SUBROUTINE AllocateMemory()
+	IMPLICIT NONE
+	ALLOCATE(U(nhy,nhx))
+	ALLOCATE(Unew(nhy,nhx))
+	ALLOCATE(Uout(nhy,nhx))
+	ALLOCATE(V(nhy,nhx))
+	ALLOCATE(Vnew(nhy,nhx))
+	ALLOCATE(Vout(nhy,nhx))
+	ALLOCATE(P(nhy,nhx))
+	ALLOCATE(Pc(nhy,nhx))
+	ALLOCATE(Pout(nhy,nhx))
+	ALLOCATE(residual(nhy,nhx))
+	ALLOCATE(div(nhy,nhx))
+	ALLOCATE(div_gl(nhy,nhx))
+	ALLOCATE(meshY(nhy))
+	ALLOCATE(delY_u(nhy,nhx))
+	ALLOCATE(delYY_u(nhy,nhx))
+	ALLOCATE(delYX_u(nhy,nhx))
+	ALLOCATE(ml(nhy,nhx))
+	ALLOCATE(tmp(nhy,nhx))
+	ALLOCATE(ae(nhx*nhy,nhx*nhy))
+    ALLOCATE(aw(nhx*nhy,nhx*nhy))
+    ALLOCATE(ap(nhx*nhy,nhx*nhy))
+    ALLOCATE(an(nhx*nhy,nhx*nhy))
+    ALLOCATE(as(nhx*nhy,nhx*nhy))
+    ALLOCATE(b(nhx,nhy))
+    
+    ALLOCATE(trial(nhy,nhx))
+    ALLOCATE(trial_lag(nhy,nhx))
+END SUBROUTINE AllocateMemory
 
+SUBROUTINE valInit()
+	IMPLICIT NONE
+	rho = 1
+    nu = 0.001
+    U_in = 60.0
+
+    ! n = 4 
+    ! x = 8 ! Domain length
+    ! y = 0.1
+    ! hx = x/2**n 
+    ! hy = y/2**n 
+    ! nhx = NINT(x/hx) + 2
+    !nhy = NINT(y/hy) + 2
+    dt = 0.00001 ! may work for bigger dt 
+
+    call userinp(x,y,hx,hy,nhx,nhy,nu,rho,U_change_max,resid_pc_max)
+
+    call Decompose(nhx,PID,np,no_node,lglel)
+
+    ll = lglel(1) 
+    lh = lglel(size(lglel))
+
+    if (PID == 0) then 
+        ll = 2
+    end if
+
+    if (PID == np - 1) lh = lh - 1
+	CALL AllocateMemory()
+	CALL linspace(-hy/2,0.1+hy/2,hy,meshY)
+
+    U = 0.0
+    U(2:nhy - 1,1:2) = U_in 
+    Unew = U
+    V = 0.0
+    Vnew = V 
+    residual = 0.0 
+    div = 0.0
+    delY_u = 0.0
+    delYY_u = 0.0
+    delYX_u = 0.0
+    P = 0.0
+    P(:,1) = 1.0
+    Pc = P 
+
+    U_change = 1
+    U_change_max = 1e-4
+    resid_pc = 1
+    resid_pc_max = 1e-4
+
+    w = 1.9
+    prs_coeff = -2/hx**2 - 2/hy**2
+    prs_coeff = 1/prs_coeff
+END SUBROUTINE valInit
+
+
+SUBROUTINE eddy_visc(nhx,nhy,Unew,delY_u,ml,delYY_u,delYX_u,ll,lh,left,right,tag,cart_comm,req)
+	
+	IMPLICIT NONE
+	INTEGER :: nhx, nhy 
+	REAL(KIND = 8),DIMENSION(nhy,nhx), INTENT(IN) :: Unew,ml
+	REAL(KIND = 8),DIMENSION(nhy,nhx), INTENT(INOUT) :: delYY_u,delYX_u,delY_u
+	INTEGER :: i,j
+	INTEGER :: ll, lh
+	INTEGER :: cart_comm,tag
+	INTEGER :: left, right,ierr, req(8)
+	
+	
+         DO i = 2,nhy - 1
+             DO j = ll,lh
+             delY_u(i,j) = (Unew(i+1,j)-Unew(i-1,j))/(2*hy)
+             end DO 
+         end DO
+         delY_u(nhy,:) = 0
+         delY_u(1,2:nhx-1) = 0
+         delY_u(2:nhy,1) = -delY_u(2:nhy,2)
+         delY_u(2:nhy,nhx) = -delY_u(2:nhy,nhx-1)
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+
+        CALL MPI_waitall(4,req,stat,ierr)
+
+        !tmp = ml**2*ABS(delY_u(:,ll:lh))*delY_u(:,ll:lh)
+        DO i = 2,nhy - 1
+            DO j = ll,lh 
+                tmp(i,j) = ml(i,j)**2*ABS(delY_u(i,j))*delY_u(i,j)
+            end DO
+        end DO
+        ! MPI send and recv
+		CALL MPI_SEND_RECV(tmp,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+        if(pid == 0)  tmp(:,1) = -tmp(:,2)
+
+        CALL MPI_waitall(4,req,stat,ierr)
+
+        DO i = 2,nhy - 1
+            DO j = ll,lh
+                delYY_u(i,j) = (tmp(i+1,j)-tmp(i-1,j))/(2*hy);
+            end DO
+        end DO
+
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delYY_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+
+        CALL MPI_waitall(4,req,stat,ierr)
+
+        DO i = 2,nhy - 1
+            DO j = ll,lh
+                delYX_u(i,j) = (tmp(i,j+1)-tmp(i,j-1))/(2*hx);
+            end DO
+        end DO
+
+		! MPI send and recv
+		CALL MPI_SEND_RECV(delYX_u,nhy,nhx,ll,lh,left,right,tag,cart_comm,req)
+
+        CALL MPI_waitall(4,req,stat,ierr)
+
+
+END SUBROUTINE eddy_visc
 
 END PROGRAM RANS_MPI 
